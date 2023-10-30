a19b91adf98829751f742a05b310327c4f9e4f4a
Kirill <ekirillo@rambler.ru>
2023-10-30 13:03:44 +0500

    Homework 6 done

---
+++ num41/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+ void CountPositiveNum(int[] array)
+{
+   int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+       if(array[i] > 0)
+       {
+        count++;
+       } 
+    } 
+    System.Console.WriteLine(count);
+}
+System.Console.Write("Input size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input array numbers: ");
+int[] array = CreateRandomArray(size);
+System.Console.Write("Введенный массив: ");
+PrintArray(array);
+System.Console.Write("Количество элементов больше нуля: ");
+CountPositiveNum(array);
---
+++ num43/Program.cs
@@ -0,0 +1,20 @@
+﻿double FindX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+    
+}
+double FindY(double x, double b1, double k1)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+System.Console.Write("Input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine($"({FindX(b1,k1,b2,k2)};{FindY(FindX(b1,k1,b2,k2), b1,k1)})");
